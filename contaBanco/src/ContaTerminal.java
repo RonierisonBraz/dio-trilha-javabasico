9a2f839a11f2d372b8ed0c5b49444e5a63e99b54
Ronierison Braz <ronierison18@gmail.com>
2024-06-19 00:12:52 -0300

    desafio finalizado

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    //TODO:conhecer e importar a class 
+     //TODO: Exibir as mensagens para o nosso usuario
+    //TODO: Obter pela scanner os valores digitados no terminal
+    //TODO: Exibir a mensagem conta criada 
+
+      int numero = 12345;
+      String agencia;
+      String nomeCliente = "Ronierison Braz";
+      double saldo = 100000.000;    
+    Scanner scan = new Scanner(System.in);
+
+     System.out.println("Por favor, digite o numero da Agência");
+      agencia = scan.nextLine();   
+    
+    
+    System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +agencia+", conta "+ numero+" e seu saldo "+saldo+" já está disponível para saque. ");
+
     }
 }
